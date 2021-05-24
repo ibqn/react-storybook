d87c69504f6d3fe0bb4c4195303541ae279cd895
ibqn <evgen.ibqn@gmail.com>
2021-05-24 11:37:01 +0200

    fix basic tests

--- src/app.test.js
+++ src/app.test.js
@@ -3,6 +3,6 @@ import App from './app'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/app/i)
+  const linkElement = screen.getByText(/App/i)
   expect(linkElement).toBeInTheDocument()
 })
